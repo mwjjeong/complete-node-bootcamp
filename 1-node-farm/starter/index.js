9d95812dd921df47d698c2a48b6619847ecba65b
Minwoo Jeong <jeongmwj@gmail.com>
2022-04-24 21:00:49 +0900

    Complete until 12. Routing

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// Blocking, synchronous way
+/* const text = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(text);
+
+const textOut = `This is what we know about the avocado: ${text}\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!"); */
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//  if (err) return console.log("ERROR! 🥲");
+//  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//    console.log(data2);
+//    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//      console.log(data3);
+//      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//        console.log("Your file has been written 😁");
+//      });
+//    });
+//  });
+//});
+
+//console.log("Reading txt/start.txt asynchronously...");
+
+// Server
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
